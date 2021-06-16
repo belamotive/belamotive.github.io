4d129930aa029dcfe0eec01723fffd1b6603a8cd
belamotive <belabankuti@Becis-MacBook-Air.local>
2021-06-16 15:43:01 +0200

    finished with all category photos

--- static/main.js
+++ static/main.js
@@ -15,17 +15,39 @@ function navScrollFunction() {
 
 const foodview = document.querySelector('#foodview');
 const enteriorview = document.querySelector('#enteriorview');
+const riportview = document.querySelector('#riportview');
+const travelview = document.querySelector('#travelview');
 
 document.querySelector('#food').addEventListener('click', () => {
   foodview.style.display = 'flex';
   enteriorview.style.display = 'none';
+  riportview.style.display = 'none';
+  travelview.style.display = 'none';
   scrollTo('foodview');
 })
 
 document.querySelector('#enterior').addEventListener('click', () => {
   foodview.style.display = 'none';
   enteriorview.style.display = 'flex';
+  riportview.style.display = 'none';
+  travelview.style.display = 'none';
   scrollTo('enteriorview');
+})
+
+document.querySelector('#riport').addEventListener('click', () => {
+  foodview.style.display = 'none';
+  enteriorview.style.display = 'none';
+  riportview.style.display = 'flex';
+  travelview.style.display = 'none';
+  scrollTo('riportview');
+})
+
+document.querySelector('#travel').addEventListener('click', () => {
+  foodview.style.display = 'none';
+  enteriorview.style.display = 'none';
+  riportview.style.display = 'none';
+  travelview.style.display = 'flex';
+  scrollTo('travelview');
 })
 
 document.querySelector('#portfolio').addEventListener('click', () => {
