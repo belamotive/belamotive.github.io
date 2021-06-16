70dae1c02606d2bbdf3fa0d05a8b85c2bba74355
belamotive <belabankuti@Becis-MacBook-Air.local>
2021-06-16 12:07:10 +0200

    added new photos

--- static/main.js
+++ static/main.js
@@ -8,10 +8,8 @@ const header = document.getElementById("header");
 function navScrollFunction() {
   if (document.body.scrollTop > 95 || document.documentElement.scrollTop > 95) {
     navbar.className = 'navbar navbar-expand-md mx-auto scrolled';
-    header.style.height = '55vh';
   } else {
     navbar.className = 'navbar navbar-expand-md notscrolled';
-    header.style.height = '80vh';
   }
 }
 
@@ -21,10 +19,22 @@ const enteriorview = document.querySelector('#enteriorview');
 document.querySelector('#food').addEventListener('click', () => {
   foodview.style.display = 'flex';
   enteriorview.style.display = 'none';
+  scrollTo('foodview');
 })
 
 document.querySelector('#enterior').addEventListener('click', () => {
   foodview.style.display = 'none';
   enteriorview.style.display = 'flex';
+  scrollTo('enteriorview');
 })
+
+document.querySelector('#portfolio').addEventListener('click', () => {
+  foodview.style.display = 'none';
+  enteriorview.style.display = 'none';
+  scrollTo('portfolio');
+})
+
+function scrollTo(hash) {
+  location.hash = "#" + hash;
+}
 
