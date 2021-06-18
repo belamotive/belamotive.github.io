9d20edf57f208332c1cca92c3f9857951d2c3293
belamotive <belabankuti@Becis-MacBook-Air.local>
2021-06-18 13:40:37 +0200

    added Title to .html

--- static/main.js
+++ static/main.js
@@ -59,10 +59,3 @@ document.querySelector('#travel').addEventListener('click', () => {
   gsap.from(".anititle", {opacity: 0, y: 100, duration: 0.5, delay: 1.5});
   gsap.from(".imgcontainer", {opacity: 0, y: -50, duration: 0.7, delay: 0.5});
 })
-
-document.querySelector('#portfolio').addEventListener('click', () => {
-  foodview.style.display = 'none';
-  enteriorview.style.display = 'none';
-  riportview.style.display = 'none';
-  travelview.style.display = 'none';
-})
